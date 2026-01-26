c07a0d2f52fdc6fadd0d8068b33d23a8f4ca45bf
Kimviruscool <sinsa9122@naver.com>
2026-01-26 22:23:29 +0900

    map

---
+++ day9/task1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+CB = pd.read_csv('CoffeeBean.csv', encoding='CP949', index_col=0, header=0, engine='python')
+# print(CB)
+addr = []
+for address in CB.address:
+    addr.append(str(address).split())
+# print(addr)
+
+addr2 = []
+
+for i in range(len(addr)) :
+    if addr[i][0] == "서울": addr[i][0]= "서울특별시"
+    elif addr[i][0] == "서울시": addr[i][0]= "서울특별시"
+    elif addr[i][0] == "부산시": addr[i][0] = "부산광역시"
+    elif addr[i][0] == "인천": addr[i][0] = "인천광역시"
+    elif addr[i][0] == "광주": addr[i][0] = "광주광역시"
+    elif addr[i][0] == "대전시": addr[i][0] = "대전광역시"
+    elif addr[i][0] == "울산시": addr[i][0] = "울산광역시"
+    elif addr[i][0] == "세종시": addr[i][0] = "세종특별자치시"
+    elif addr[i][0] == "경기": addr[i][0] = "경기도"
+    elif addr[i][0] == "충북": addr[i][0] = "충청북도"
+    elif addr[i][0] == "충남": addr[i][0] = "충청남도"
+    elif addr[i][0] == "전북": addr[i][0] = "전라북도"
+    elif addr[i][0] == "전남": addr[i][0] = "전라남도"
+    elif addr[i][0] == "경북": addr[i][0] = "경상북도"
+    elif addr[i][0] == "경남": addr[i][0] = "경상남도"
+    elif addr[i][0] == "제주": addr[i][0] = "제주특별자치도"
+    elif addr[i][0] == "제주도": addr[i][0] = "제주특별자치도"
+    elif addr[i][0] == "제주시": addr[i][0] = "제주특별자치도"
+
+    addr2.append(''.join(addr[i]))
+
+print(addr2)
+
+addr2 = pd.DataFrame(addr2, columns=['address2'])
+CB2 = pd.concat([CB,addr2], axis=1)
+CB2.head()
+
+CB2.to_csv('CoffeeBean2.csv', encoding='CP949', index=False)
+
+import folium
+
+map_osm = folium.Map(location=[37.5599,126.9754], zoom_start=16)
+map_osm.save('map.html')
