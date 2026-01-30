8fe2319fe98b7e666510c5a7575ed9a113b74416
Kimviruscool <sinsa9122@naver.com>
2026-01-30 21:50:10 +0900

    map

---
+++ day10/Task1.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import fetch_california_housing
+housing = fetch_california_housing()
+
+print(housing.DESCR)
+
+housing_df = pd.DataFrame(housing.data, columns=housing.feature_names)
+print(housing_df.head())
+
+housing_df['PRICE'] = housing.target
+print(housing_df.head())
+
+print("캘리포니아 주택 가격 데이터셋 크기 :", housing_df.shape)
+print(housing_df.info(),"\n")
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+Y = housing_df['PRICE']
+X = housing_df.drop('PRICE', axis=1, inplace=False)
+
+x_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=156)
+
+lr = LinearRegression()
+
+lr.fit(x_train, y_train)
+
+Y_predict = lr.predict(X_test)
+
+mse = mean_squared_error(y_test, Y_predict)
+rmse = np.sqrt(mse)
+
+print('MSE : {0:3f}, RMSE : {1:3f}'.format(mse, rmse))
+print('R^2(Varicance score) : {0:3f}'.format(r2_score(y_test, Y_predict)))
+
+print("Y절편 값 : ",lr.intercept_)
+print("회귀 계수 값 : ", np.round(lr.coef_, 1))
+
+coef = pd.Series(data = np.round(lr.coef_,2), index = X.columns)
+print(coef.sort_values(ascending = False))
+
