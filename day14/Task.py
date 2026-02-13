ed7ca3441f8c4dd2826e4b6ef409010379b06906
Kimviruscool <sinsa9122@naver.com>
2026-02-13 13:19:02 +0900

    task

--- day14/Task.py
+++ day14/Task.py
@@ -61,3 +61,71 @@ plt.show()
 
 import numpy as np
 
+customer_df['Freq_log'] = np.log1p(customer_df['Freq'])
+customer_df['SaleAmount_log'] = np.log1p(customer_df['SaleAmount'])
+customer_df['ElapsedDays_log'] = np.log1p(customer_df['ElapsedDays'])
+print(customer_df.head())
+
+fig,ax = plt.subplots()
+ax.boxplot([customer_df['Freq_log'],customer_df['SaleAmount_log'],customer_df['ElapsedDays_log']], sym='bo')
+plt.xticks([1,2,3],['Freq_log','SaleAmount_log','ElapsedDays_log'])
+plt.show()
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, silhouette_samples
+
+X_feature = customer_df[['Freq_log','SaleAmount_log','ElapsedDays_log']].values
+
+from sklearn.preprocessing import StandardScaler
+X_feature_scaled = StandardScaler().fit_transform(X_feature)
+
+distortions = []
+
+for i in range(1, 11):
+    kmeans_i = KMeans(n_clusters=i, random_state = 0)
+    kmeans_i.fit(X_feature_scaled)
+    distortions.append(kmeans_i.inertia_)
+
+plt.plot(range(1, 11), distortions, marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distortion')
+plt.show()
+
+kmeans = KMeans(n_clusters=3, random_state=0)
+Y_labels = kmeans.fit_predict(X_feature_scaled)
+
+customer_df['ClusterLabel'] = Y_labels
+print(customer_df.head())
+
+from matplotlib import cm
+
+def silhoutteViz(n_cluster, X_features):
+    kmeans = KMeans(n_clusters=n_cluster, random_state=0)
+    Y_labels = kmeans.fit_predict(X_features)
+
+    silhouette_values = silhouette_score(X_features, Y_labels, metric='euclidean')
+
+    y_ax_lower, y_ax_upper = 0,0
+    y_ticks = []
+
+    for c in range(n_cluster):
+        c_silhouettes = silhouette_values[Y_labels == c]
+        c_silhouettes.sort()
+        y_ax_upper += len(c_silhouettes)
+        color = cm.jet(float(c)/n_cluster)
+        plt.barh(range(y_ax_lower, y_ax_upper), c_silhouettes, height=1.0, edgecolor='none', color=color)
+        y_ticks.append((y_ax_lower + y_ax_upper) / 2.)
+        y_ax_lower += len(c_silhouettes)
+
+    silhouette_avg = np.mean(silhouette_values)
+    plt.axvline(x=silhouette_avg, color='k', linestyle='--')
+    plt.title('Number of Cluster : '+ str(n_cluster) + '\n' + 'Silhouette Score:'+str(round(silhouette_avg,3)))
+    plt.yticks(y_ticks,range(n_cluster))
+    plt.xticks([0,0.2,0.4,0.6,0.8,1])
+    plt.ylabel('Cluster')
+    plt.xlabel('Silhouette coefficient')
+    plt.tight_layout()
+    plt.show()
+
+def clusterScatter(n_cluster, X_features):
+    print('ing')
